32acd2c3d60214039007db62daa70396b48821d0
Gafsi mohamed salem <salemgafsi79@gmail.com>
2026-04-23 09:43:06 +0100

    Update environment.staging.ts

--- thunder-mobile-stage/src/environments/environment.staging.ts
+++ thunder-mobile-stage/src/environments/environment.staging.ts
@@ -3,8 +3,8 @@ const API_URL = '';
  
 export const environment = {
    production: false,
-  geminiApiKey: 'AIzaSyBvVM6y0pZoJCch8R2Q4dv-Dr27w1OG-P8',             // ← Coller votre clé Google AI Studio
-  geminiModel: 'gemini-2.5-flash-lite',           // Modèle gratuit disponible sur votre compte
-  apiUrl: 'http://localhost:8000/api/client',
+  geminiApiKey: '',             // ← Coller votre clé Google AI Studio
+  geminiModel: '',           // Modèle gratuit disponible sur votre compte
+  apiUrl: '',
 };
 
