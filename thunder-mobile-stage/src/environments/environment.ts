106cb45c0dad54e61a7adf9ed97e48b3e749b283
Gafsi mohamed salem <salemgafsi79@gmail.com>
2026-04-23 09:43:29 +0100

    Update environment.ts

--- thunder-mobile-stage/src/environments/environment.ts
+++ thunder-mobile-stage/src/environments/environment.ts
@@ -3,7 +3,7 @@ const API_URL = '';
 
  export const environment = {
   production: false,
-  geminiApiKey: 'AIzaSyDYUf4lthDTt7f7J4peSFepuObdD0vxiEg',             // ← Coller votre clé Google AI Studio
-  geminiModel: 'gemini-2.5-flash-lite',           // Modèle gratuit disponible sur votre compte
-  apiUrl: 'http://localhost:8000/api/client',      // URL de base de votre API Laravel
+  geminiApiKey: '',             // ← Coller votre clé Google AI Studio
+  geminiModel: '',           // Modèle gratuit disponible sur votre compte
+  apiUrl: '',      // URL de base de votre API Laravel
 };
